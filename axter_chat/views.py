ebeca30e81d027e46ea34e8a9eff7b7075bb4e99
ashwin <123428347+ashwiniso@users.noreply.github.com>
2023-07-19 10:20:50 +0100

    remodded files

--- axter_chat/views.py
+++ axter_chat/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the axter_chat index.")
+    return render(request, "axter_chat/index.html")
 
 def specific(request):
     return HttpResponse("Hello, world. You're at the axter_chat specific.")
-
-def article(request, article_id):
-    return render(request, "axter_chat/index.html", {"article_id": article_id})
