1072fd2cbee91a41c7b91df38fd1a2dc08c06f32
ashwin <123428347+ashwiniso@users.noreply.github.com>
2023-08-07 04:43:55 +0100

    add the chatterbot lib and setup list training

--- axter_chat/views.py
+++ axter_chat/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot('Axter',read_only=False,logic_adapters=['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.BestMatch'])
+
+
+list_to_train = [
+    "Hi",
+    "Hello",
+    "How are you?",
+    "I am good.",
+    "That is good to hear.",
+    "Thank you",
+    "What is your name?",
+    "My name is Axter.",
+    "Who are you?",
+    "I am Axter.",
+    "Who created you?",
+    "I was created by a dev who goes by ashwiniso",
+    "What is your purpose?",
+    "To help you with your doubts",
+]
+
+
 def index(request):
     return render(request, "axter_chat/index.html")
 
